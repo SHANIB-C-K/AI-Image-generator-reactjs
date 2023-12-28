9b9cc89fa9a545896174e4b765e40243b7d29629
SHANIB C K <ckshanib8@gmail.com>
2023-12-28 13:17:30 +0530

    sample created

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,39 @@
-import React from "react";
-import.meta.env.VITE_MY_API_KEY;
+import React, { useRef, useState } from "react";
 import def_img from "../assets/default_image.svg";
+const key = import.meta.env.VITE_MY_API_KEY;
 
 const HomePage = () => {
+  const [Image, setImage] = useState("/");
+  const [IsLoading, setIsLoading] = useState(false);
+  let inputRef = useRef(null);
+
+  const ImageGenerator = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${key}`,
+          "User-Agent": "Brave",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: "512x512",
+        }),
+      }
+    );
+    let data = await response.json();
+    console.log(data);
+    setIsLoading(false);
+    let data_array = data.data;
+    setImage(data_array[0].url);
+    setIsLoading(false);
+  };
   return (
     <>
       <div className="h-screen w-screen bg-gray-800 flex items-center justify-center">
@@ -10,10 +41,27 @@ const HomePage = () => {
           <h1 className="text-white">
             Ai Image <span>Generator</span>
           </h1>
-          <img src={def_img} alt="" />
+          {IsLoading ? (
+            <div>
+              <p className="text-gray-500">Loading...</p>
+              <p className="text-gray-500">
+                Please wait until your image is ready
+              </p>
+            </div>
+          ) : (
+            <img src={Image === "/" ? def_img : Image} alt="" />
+          )}
+
           <div className="pt-5">
-            <input type="text" />
-            <input type="submit" className="bg-green-800" />
+            <input type="text" ref={inputRef} />
+            <input
+              type="submit"
+              className="bg-green-800"
+              onClick={() => {
+                setIsLoading(true);
+                ImageGenerator();
+              }}
+            />
           </div>
         </div>
       </div>
