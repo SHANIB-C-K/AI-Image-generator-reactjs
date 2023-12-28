c4aef8c34b8053bb4daba2c78804183cfc0324d7
SHANIB C K <ckshanib8@gmail.com>
2023-12-28 11:49:42 +0530

    default image added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import.meta.env.VITE_MY_API_KEY;
-import { ConflictError, OpenAI } from "openai";
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="h-screen w-screen bg-gray-800 flex items-center justify-center"></div>
+      <div>
+        <img src= alt="" />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
