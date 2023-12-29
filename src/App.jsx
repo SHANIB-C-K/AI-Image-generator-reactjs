87ccb4be0abd0c94924bc10c09beaadce8a2edb0
SHANIB C K <ckshanib8@gmail.com>
2023-12-29 21:57:30 +0530

    commands added at App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+// This is App.jsx file
+// This is importing section
 import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
+      {/* This is HomePage  */}
       <HomePage />
     </>
   );
